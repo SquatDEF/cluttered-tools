81e698ece0a8285a74d2be16782e78ed1b2c4fa5
SquatDEF <squat-def@qq.com>
2025-05-23 18:38:54 +0800

    添加开放空间logo，主页写了一点

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import overFieldLogo from '@of/assets/logo.png';
+
+export const projectsConfig =  [
+  {
+    name: '开放空间',
+    pathName: 'over-field',
+    img: overFieldLogo,
+  }
+]
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+import { projectsConfig } from '@/config'
+
+</script>
+
 <template>
-  2333
+  <div class="home">
+    <div class="home__list">
+      <div v-for="(project, index) in projectsConfig" :key="index" class="home__item">
+        <img :src="project.img" alt="" />
+        <span>{{ project.name }}</span>
+      </div>
+    </div>
+  </div>
 </template>
